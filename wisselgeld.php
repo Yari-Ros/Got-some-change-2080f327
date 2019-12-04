5252bd7c2a23a6f476a767d52a7b7ea348c12acb
Yari-Ros <2118088@talnet.nl>
2019-12-04 10:09:40 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,9 @@
+<?php
+$getal = $argv[1];
+if ($getal >= 1) {
+    echo intval ($getal), " x 1 Euro";
+}   else {
+        echo "Geen wisselgeld!";
+}
+?>  
+<!-- This code was writtin by Yari, Do not use! -->
